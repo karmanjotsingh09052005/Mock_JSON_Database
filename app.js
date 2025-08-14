b0ad636dc77f684aae8553a411ba10baac355227
karmanjotsingh09052005 <09karmanjot2005@gmail.com>
2025-08-13 18:25:33 -0600

    Update

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT;
+const uri = process.env.MONGO_URI;
+
+
+
+mongoose.connect(uri).then(
+    async()=>{
+        console.log('connected to MongoDB.')
+    
+
+app.listen(PORT,()=>{
+    console.log(`connected to port ${PORT}`);
+});
+    }
+).catch((err) =>{console.log('Error: ${error}')});
